a89674b9df706ee338f3ebab339143d853a55d1a
himajin-vip <60175976+himajin-vip@users.noreply.github.com>
2020-02-05 01:27:05 +0900

    移動

--- PlayerManager/PlayerObj.cs
+++ PlayerManager/PlayerObj.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class PlayerObj : MonoBehaviour
 {
-  
     void OnTriggerEnter2D(Collider2D collision){
       if(collision.gameObject.tag == "Item"){
         PlayerManager.ItemGet(collision);
