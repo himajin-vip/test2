cf61c64aee8b3fc39e54dbd0ed996e92dc02215a
himajin-vip <60175976+himajin-vip@users.noreply.github.com>
2020-02-07 23:37:53 +0900

    削除

--- AudioManager/AudioPlayer.cs
+++ AudioManager/AudioPlayer.cs
@@ -5,14 +5,7 @@ using UnityEngine;
 public class AudioPlayer : MonoBehaviour
 {
   public AudioSource MySE;
-  public AudioPlayer(string SEName){
-    // Debug.Log("test");
-    // AudioClip AC;
-    // AC = (AudioClip)Resources.Load("SE/"+SEName);
-    // MySE = gameObject.AddComponent<AudioSource>();
-    // MySE.clip = AC;
 
-  }
   public void Play(){
     MySE.Play();
   }
