b844d7dbb1f0c288eab0e1a68cff92e2a7a2ad78
himajin-vip <60175976+himajin-vip@users.noreply.github.com>
2020-02-13 07:58:43 +0900

    Inventoryの追加をエクササイズ！

--- Inventory/Inventory.cs
+++ Inventory/Inventory.cs
@@ -6,26 +6,21 @@ public class Inventory
 {
     public List<int> ItemIDList{get; private set;} = new List<int>();
     public List<int> ItemPeaceList{get; private set;} = new List<int>();
+    Dictionary<int,int> List = new Dictionary<int, int>();
 
     public void Add(int addItemNo,int addItemPeace){
+        List<int> Idlist = new List<int>(List.Keys);
         bool NewItem = true;
-        int count = 0;
-        foreach(int ItemID in ItemIDList){
-            if( ItemID == addItemNo ){
-                NewItem = false;
-                break;
-            }
-            count++;
-        }
+        NewItem = Idlist.Contains(addItemNo);
+   
         if(NewItem){
-            ItemIDList.Add(addItemNo);
-            ItemPeaceList.Add(addItemPeace);
-        }else{
-            ItemPeaceList[count] += addItemPeace;
+            List.Add(addItemNo,addItemPeace);
+        }
+        if(!NewItem){
+            List[addItemNo] = addItemPeace;
         }
 
     }
-    
 
     public void Reduce(int reduceItemNo,int reduceItemPeace){
         for(int i = 0;i<ItemIDList.Count;i++){
