ce62133f8445020f6e953b45baafc4f94f7e265d
himajin-vip <60175976+himajin-vip@users.noreply.github.com>
2020-02-13 09:38:56 +0900

    ItemTypeを検索するクラスを作成！

---
+++ Item/ItemLibrary.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+public class ItemLibrary
+{
+    private Dictionary<int,ItemName> ItemList = new Dictionary<int, ItemName>();
+
+    public ItemLibrary(){
+        ItemList.Add(0,ItemName.Potion);
+        ItemList.Add(1,ItemName.HiPotion);
+        ItemList.Add(100,ItemName.IronSword);
+        ItemList.Add(200,ItemName.LeatherCap);
+        ItemList.Add(300,ItemName.LeatherBody);
+        ItemList.Add(400,ItemName.LeatherHand);
+        ItemList.Add(500,ItemName.LeatherFoot);
+    }
+
+    public ItemType GetItemType(ItemID itemID){
+        ItemName itemname = ItemList[itemID.GetID()];
+        Type itemtype = Type.GetType(itemname.ToString());
+        Item item = (Item)Activator.CreateInstance(itemtype);
+        return item.Type;
+    }
+
+}
