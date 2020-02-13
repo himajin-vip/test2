bd9a3d53fd36424b7ef2c8b4dd7b0257cbb67424
himajin-vip <60175976+himajin-vip@users.noreply.github.com>
2020-02-13 22:06:14 +0900

    FirstIntClassを文字列に変更する人！

---
+++ Athor/FistintClasstoStringer.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstintClasstoStringer
+{
+    public string Get(FirstIntClass firstIntClass){
+        string text;
+        int value = firstIntClass.GetValue();
+        text = value.ToString();
+        return text;
+    }
+}
