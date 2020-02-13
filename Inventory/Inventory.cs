d0f85ca8ff00f1a941a9b26505c0c55ad8b65380
himajin-vip <60175976+himajin-vip@users.noreply.github.com>
2020-02-13 09:28:40 +0900

    ラップ！！！

--- Inventory/Inventory.cs
+++ Inventory/Inventory.cs
@@ -6,35 +6,35 @@ public class Inventory
 {
     private Dictionary<int,int> List = new Dictionary<int, int>();
 
-    public void Add(int addItemNo,int addItemPeace){
-        if(HaveCheck(addItemNo)){
-            List.Add(addItemNo,addItemPeace);
+    public void Add(ItemID itemID,ItemPeace itemPeace){
+        if(HaveCheck(itemID)){
+            List.Add(itemID.GetID(),itemPeace.GetPeace());
         }
-        if(!HaveCheck(addItemNo)){
-            List[addItemNo] = addItemPeace;
-        }
-    }
-    public void Reduce(int reduceItemNo,int reduceItemPeace){
-        if(HaveCheck(reduceItemNo)){
-            List[reduceItemNo] -= reduceItemPeace;
-            RemoveItem(reduceItemNo);
+        if(!HaveCheck(itemID)){
+            List[itemID.GetID()] = itemPeace.GetPeace();
         }
     }
-    public void RemoveItem(int itemid){
-        if(List[itemid]<=0){
-            List.Remove(itemid);
+    public void Reduce(ItemID itemID,ItemPeace itemPeace){
+        if(HaveCheck(itemID)){
+            List[itemID.GetID()] -= itemPeace.GetPeace();
+            RemoveItem(itemID);
+        }
+    }
+    public void RemoveItem(ItemID itemID){
+        if(List[itemID.GetID()]<=0){
+            List.Remove(itemID.GetID());
         }
     }
 
-    public int ReturnPeace(int ItemID){
-        if(HaveCheck(ItemID)){
-            return List[ItemID];
+    public int ReturnPeace(ItemID itemID){
+        if(HaveCheck(itemID)){
+            return List[itemID.GetID()];
         }
         return 0;        
     }
-    public bool HaveCheck(int ItemID){
+    public bool HaveCheck(ItemID itemID){
         List<int> Idlist = new List<int>(List.Keys);
-        return Idlist.Contains(ItemID);
+        return Idlist.Contains(itemID.GetID());
     }
     public void Load(List<int> itemIDList, List<int> itemPeaceList){
         for(int i = 0;i<itemIDList.Count;i++){
