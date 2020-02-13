0a1c4bec8e94903e6af8b459df784f0278e30fc7
himajin-vip <60175976+himajin-vip@users.noreply.github.com>
2020-02-13 09:08:46 +0900

    アイテムのIDと個数をラップしたよ

---
+++ Item/ItemBag.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemBag
+{
+    private int ItemID;
+    private int Peace;
+
+    ItemBag(int itemid, int peace){
+        ItemID = itemid;
+        Peace = peace;
+    }
+    public int Getid(){
+        return ItemID;
+    }
+    public int GetPeace(){
+        return Peace;
+    }
+}
