018f5ce196a453a3e26ce044111a845312a024d5
himajin-vip <60175976+himajin-vip@users.noreply.github.com>
2020-02-14 08:47:56 +0900

    ItemID,ItemPeaceをまとめたよ

---
+++ Item/ItemBag.cs
@@ -0,0 +1,11 @@
+﻿
+public class ItemBag
+{
+    private ItemID itemID;
+    private ItemPeace itemPeace;
+
+    public ItemBag(ItemID itemid ,ItemPeace itempeace){
+        itemID = itemid;
+        itemPeace = itempeace;
+    }
+}
